8c3428200942a3fe0e895b9fe257247bae993ab3
Alex <61236755+SeclaPanda@users.noreply.github.com>
2023-02-20 20:58:59 +0300

    add few new sol

---
+++ Ransom_note.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        return st1 & st2 == st1
